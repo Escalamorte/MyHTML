133557fb0c90d269e9d03ac607963f9544c6a1c3
escalamorte <elnurdadashev@gmail.com>
2019-07-24 14:27:16 +0400

    calc

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ $(".navbar a").click(function(){
         scrollTop:$("#" + $(this).data('value')).offset().top
     },5000)
 });
+function display(num) {
+    document.form.screen.value = document.form.screen.value + num;
+    equal();
+}
+function clearAll() {
+    document.form.screen.value = "";
+    document.form.result.value = "";
+}
+
+function equal() {
+    let exp = document.form.screen.value;
+    if(exp){
+        document.form.result.value = eval(exp);
+    }
+
+    let value = document.form.screen.value;
+    var length = value.length - 1;
+
+    console.log("Length is " + length);
+    console.log("charAt is " + value.charAt(length));
+}
+function backspace() {
+    var exp = document.form.screen.value;
+    document.form.screen.value = exp.substring(0, exp.length-1);
+    equal();
+}
